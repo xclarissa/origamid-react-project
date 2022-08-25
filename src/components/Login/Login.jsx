d234a7ba00b7e1e6ce81be9ec1553fe7ed9728a4
xclarissa <clarissaxr@gmail.com>
2022-08-25 11:05:41 -0300

    Estilo tela Login,userContext, link esqueceu senha e cadastro

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import LoginForm from '../Login/LoginForm'
-import LoginCreate from '../Login/LoginCreate'
-import LoginPasswordLost from '../Login/LoginPasswordLost'
-import LoginPasswordReset from '../Login/LoginPasswordReset'
-import { UserContext } from '../../contexts/UserContext'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginForm from "../Login/LoginForm";
+import LoginCreate from "../Login/LoginCreate";
+import LoginPasswordLost from "../Login/LoginPasswordLost";
+import LoginPasswordReset from "../Login/LoginPasswordReset";
+import { UserContext } from "../../contexts/UserContext";
+import styles from "./Login.module.css";
 
 function Login() {
-  const { login } = React.useContext(UserContext) 
+  const { login } = React.useContext(UserContext);
 
-  if(login === true) return <Navigate to='/my-account' />
- 
+  if (login === true) return <Navigate to="/my-account" />;
+
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<LoginForm />} />
-        <Route path='criar' element={<LoginCreate />} />
-        <Route path='perdeu' element={<LoginPasswordLost />} />
-        <Route path='resetar' element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
-  )
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="signup" element={<LoginCreate />} />
+          <Route path="lost-password" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
+  );
 }
 
-export default Login
+export default Login;
--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -4,6 +4,9 @@ import Input from "../../components/Forms/Input";
 import Button from "../../components/Forms/Button";
 import useForm from "../../hooks/useForm";
 import { UserContext } from "../../contexts/UserContext";
+import Error from "../Helper/Error";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../../components/Forms/Button.module.css";
 
 function LoginForm() {
   const username = useForm();
@@ -21,13 +24,28 @@ function LoginForm() {
   return (
     <section className="animeLeft">
       <h1 className="title">Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        {loading ? <Button disabled>Carregando...</Button> : <Button>Entrar</Button>}
-        {error && <p>{error}</p>}
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+
+        <Error error={error} />
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+
+      <Link className={styles.lost} to="/login/lost-password">
+        Esqueceu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to="/login/signup">
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 }
